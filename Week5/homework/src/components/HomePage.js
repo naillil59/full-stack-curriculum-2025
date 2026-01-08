f740cef9f733ef76c8f523ce6117ed102f9fe66c
Lillian Wu <lillianwu72@gmail.com>
2026-01-08 15:12:51 -0600

    updated frontend

--- Week5/homework/src/components/HomePage.js
+++ Week5/homework/src/components/HomePage.js
@@ -33,7 +33,7 @@ export default function HomePage() {
     if (!currentUser) {
       navigate("/login")
     } else {
-      fetch(`http://localhost:3001/tasks/${currentUser.uid}`)
+      fetch(`process.env.REACT_APP_BACKEND/tasks/${currentUser.uid}`)
         .then((response) => response.json())
         .then((data) => {
           setTaskList(data)
@@ -52,7 +52,7 @@ export default function HomePage() {
       // In addition to updating the state directly, you should send a request
       // to the API to add a new task and then update the state based on the response.
 
-      fetch("http://localhost:3001/tasks", {
+      fetch("process.env.REACT_APP_BACKEND/tasks", {
         method: "POST",
         headers: {
           'Content-Type': "application/json"
@@ -81,7 +81,7 @@ export default function HomePage() {
     // TODO: Support removing/checking off todo items in your todo list through the API.
     // Similar to adding tasks, when checking off a task, you should send a request
     // to the API to update the task's status and then update the state based on the response.
-    fetch(`http://localhost:3001/tasks/${task.id}`, {
+    fetch(`process.env.REACT_APP_BACKEND/tasks/${task.id}`, {
       method: "DELETE"
     })
     .then(response => response.json())
